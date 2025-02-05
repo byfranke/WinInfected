012956bcc38f799689674fb05dfb38561b011311
Frank E <131370932+byfranke@users.noreply.github.com>
2025-02-05 04:23:22 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,20 @@
-sudo chmod +x WinInfected
-sudo cp WinInfected /usr/bin
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+if [ -f "WinInfected" ]; then
+  sudo chmod +x wininfected
+else
+  echo "Error: WinInfected file not found."
+  exit 1
+fi
+
+if sudo cp wininfected /usr/bin/; then
+  echo "WinInfected installed successfully."
+else
+  echo "Error: Failed to copy WinInfected to /usr/bin."
+  exit 1
+fi
